3c7f956ec155e5cda9a195e86865808a49a4f1b3
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-06 14:51:42 -0400

    Camera Smoothing.

--- Scenes/game_camera/game_camera.gd
+++ Scenes/game_camera/game_camera.gd
@@ -1,12 +1,18 @@
 extends Camera2D
 
+var target_position = Vector2.ZERO
+const SMOOTH = 10
+
 func _ready():
     make_current()
 
 func _process(delta):
+    acquire_target()
+    global_position = global_position.lerp(target_position, 1.0 - exp( - delta * SMOOTH))
+
+func acquire_target():
     var player_nodes = get_tree().get_nodes_in_group("player")
     if player_nodes.size() > 0:
         var player = player_nodes[0] as Node2D
-        print(player.global_position)
-        global_position = player.global_position
+        target_position = player.global_position
         
