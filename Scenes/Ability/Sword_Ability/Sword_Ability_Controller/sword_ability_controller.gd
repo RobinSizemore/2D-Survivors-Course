ca6b81fa6000049b4bf2b87b586d9e1d41f49f2f
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-06 20:09:49 -0400

    Sword "attacks" enemy.

--- Scenes/Ability/Sword_Ability/Sword_Ability_Controller/sword_ability_controller.gd
+++ Scenes/Ability/Sword_Ability/Sword_Ability_Controller/sword_ability_controller.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@export var max_range: float = 100
 @export var sword_ability: PackedScene
 
 func _ready():
@@ -9,6 +10,17 @@ func on_timer_timeout():
     var player = get_tree().get_first_node_in_group("player") as Node2D
     if player == null:
         return
+    var enemies = get_tree().get_nodes_in_group("enemies")
+    enemies = enemies.filter(func(enemy: Node2D):
+        # Checking squared distance is mathematically faster than just distance (for some reason)
+        return enemy.global_position.distance_squared_to(player.global_position) < pow(max_range, 2)
+    )
+    if enemies.size() < 1: # No enemies in range,
+        return
+    enemies.sort_custom(func(a: Node2D, b: Node2D):
+        return a.global_position.distance_squared_to(player.global_position) < b.global_position.distance_squared_to(player.global_position)
+    )
+    
     var sword_instance = sword_ability.instantiate() as Node2D
     player.get_parent().add_child(sword_instance)
-    sword_instance.global_position = player.global_position
+    sword_instance.global_position = enemies[0].global_position
