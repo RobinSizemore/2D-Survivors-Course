45ff7dae969d89e3edbf8d5d00c69fca61761805
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-09 18:22:17 -0400

    Experience bars and level ups (no actual effect, but "Sword Quickness" upgrades accumulate.

--- Scenes/Manager/experience_manager.gd
+++ Scenes/Manager/experience_manager.gd
@@ -1,15 +1,27 @@
 extends Node
 
+signal experience_updated(current_experience: float, target_experience: float)
+signal level_up(new_level: int)
+
+const TARGET_EXPERIENCE_GROWTH = 5
+
 var current_experience = 0
+var current_level = 1
+var target_experience = 5
 
 func _ready():
     GameEvents.exp_vial_collected.connect(on_experience_vial_collected)
 
 func increment_experience(number: float):
-    current_experience += number
-    print("Experience: ", current_experience)
-    
+    current_experience = min(current_experience + number, target_experience)
+    experience_updated.emit(current_experience, target_experience)
+    if current_experience >= target_experience:
+        current_experience = 0
+        current_level += 1
+        target_experience += TARGET_EXPERIENCE_GROWTH
+        experience_updated.emit(current_experience, target_experience)
+        level_up.emit(current_level)
+        
 func on_experience_vial_collected(number: float):
-    print("Signal received")
     increment_experience(number)
     
