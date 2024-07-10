b00bc1c320078061bb13d144e4b93dbc599134f2
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-10 12:50:56 -0400

    Victory Scene!

--- Scenes/Manager/arena_time_manager.gd
+++ Scenes/Manager/arena_time_manager.gd
@@ -1,6 +1,15 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
 @onready var arenaTimer = $Timer
+
+func _ready():
+    arenaTimer.timeout.connect(on_arena_timer_timeout)
+    arenaTimer.start()
 
 func get_time_elapsed():
     return arenaTimer.wait_time - arenaTimer.time_left
+
+func on_arena_timer_timeout():
+    var victory_screen = victory_screen_scene.instantiate()
+    get_tree().current_scene.add_child(victory_screen)
---
+++ Scenes/UI/victory_screen.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+func _ready():
+	get_tree().paused = true
+	$%RestartButton.pressed.connect(on_restart_button_pressed)
+	$%QuitButton.pressed.connect(on_quit_button_pressed)
+
+func on_restart_button_pressed():
+	var tree = get_tree()
+	get_tree().paused = false
+	if tree:
+		get_tree().change_scene_to_file("res://Scenes/Main/main.tscn")
+	else:
+		print("Error: Tree is null.")
+
+func on_quit_button_pressed():
+	get_tree().quit()
