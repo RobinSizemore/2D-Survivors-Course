bbd07b69548a79b31689e576f9cc00c6818f6ecc
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-10 18:11:26 -0400

    Our arena has walls, and enemies can't spawn outside.

--- Scenes/Manager/enemy_manager.gd
+++ Scenes/Manager/enemy_manager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const TERRAIN_PHYSICS_LAYER = 1
 const SPAWN_RADIUS = 350
 @export var basic_enemy_scene: PackedScene
 @export var arena_time_manager: Node
@@ -12,19 +13,36 @@ func _ready():
     enemy_timer.timeout.connect(on_timer_timeout)
     arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
+func get_spawn_position():
+    var player = get_tree().get_first_node_in_group("player") as Node2D
+    if player == null:
+        return Vector2.ZERO
+    var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+    var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+    
+    for i in range(4):
+        # Is there a wall between the player and the spawn position of the enemy?
+        var query_params = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, TERRAIN_PHYSICS_LAYER)
+        var collision_result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_params)
+    
+        if collision_result.is_empty(): # If not, spawn the enemy.
+            break
+        else: # If so, rotate and try again.
+            random_direction = random_direction.rotated(TAU / 4)
+            spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+        
+    return spawn_position
+
 func on_timer_timeout():
     var basic_enemy_instance = basic_enemy_scene.instantiate() as Node2D
     var player = get_tree().get_first_node_in_group("player") as Node2D
     if player == null:
         return
-    var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-    var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
-    
     var entities_layer = get_tree().get_first_node_in_group("entities_layer")
     if entities_layer == null:
         print("No entities layer found!")
         return
-    basic_enemy_instance.global_position = spawn_position
+    basic_enemy_instance.global_position = get_spawn_position()
     entities_layer.add_child(basic_enemy_instance)
     enemy_timer.start()
 
