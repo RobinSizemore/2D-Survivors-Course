207d76f6a6bd12312ed8ee491fb44f3ab7fc1f06
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-15 17:55:29 -0400

    Upgrade screen... um... upgrades.

--- Scenes/Components/ability_upgrade_card.gd
+++ Scenes/Components/ability_upgrade_card.gd
@@ -5,8 +5,19 @@ signal ability_upgrade_selected
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel
 
+var disabled = false
+
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+	
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+	
+func play_discard():
+	$AnimationPlayer.play("discard")
 
 func set_ability_upgrade(ability_upgrade: AbilityUpgrade):
 	if name_label == null or description_label == null:
@@ -15,7 +26,26 @@ func set_ability_upgrade(ability_upgrade: AbilityUpgrade):
 	name_label.text = ability_upgrade.name
 	description_label.text = ability_upgrade.description
 
+func select_card():
+	disabled = true
+	$AnimationPlayer.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_cards"):
+		if other_card == self:
+			continue
+		other_card.disabled = true
+		other_card.play_discard()
+	
+	await $AnimationPlayer.animation_finished
+	ability_upgrade_selected.emit()
+
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
 	if event.is_action_pressed("left_click"):
-		print("Clicked on card!")
-		ability_upgrade_selected.emit()
+		select_card()
+
+func on_mouse_entered():
+	if disabled:
+		return
+	$HoverAnimationPlayer.play("hover")
--- Scenes/UI/upgrade_screen.gd
+++ Scenes/UI/upgrade_screen.gd
@@ -9,13 +9,18 @@ func _ready():
 	get_tree().paused = true
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var i = 0
 	for upgrade in upgrades:
 		var card = upgrade_card_scene.instantiate()
 		card_container.add_child(card)
 		card.set_ability_upgrade(upgrade)
+		card.play_in(0.25 * float(i))
 		card.ability_upgrade_selected.connect(on_ability_upgrade_selected.bind(upgrade))
+		i = i + 1
 
 func on_ability_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)
+	$AnimationPlayer.play("out")
+	await $AnimationPlayer.animation_finished
 	get_tree().paused = false
 	queue_free()
