7b417788b6be03cdd77b13fac96ab05325505540
Robin Sizemore <robin.b.sizemore@outlook.com>
2024-07-15 17:15:22 -0400

    Fixing Mouse Filter on Upgrade Card

--- Scenes/Components/ability_upgrade_card.gd
+++ Scenes/Components/ability_upgrade_card.gd
@@ -6,16 +6,16 @@ signal ability_upgrade_selected
 @onready var description_label: Label = $%DescriptionLabel
 
 func _ready():
-    gui_input.connect(on_gui_input)
+	gui_input.connect(on_gui_input)
 
 func set_ability_upgrade(ability_upgrade: AbilityUpgrade):
-    if name_label == null or description_label == null:
-        print("Name or Description label not found!")
-        return
-    name_label.text = ability_upgrade.name
-    description_label.text = ability_upgrade.description
+	if name_label == null or description_label == null:
+		print("Name or Description label not found!")
+		return
+	name_label.text = ability_upgrade.name
+	description_label.text = ability_upgrade.description
 
 func on_gui_input(event: InputEvent):
-    if event.is_action_pressed("left_click"):
-        print("Clicked on card!")
-        ability_upgrade_selected.emit()
+	if event.is_action_pressed("left_click"):
+		print("Clicked on card!")
+		ability_upgrade_selected.emit()
--- Scenes/Main/main.gd
+++ Scenes/Main/main.gd
@@ -3,10 +3,10 @@ extends Node
 @export var end_screen_scene: PackedScene
 
 func _ready():
-    $%Player.health_component.died.connect(on_player_died)
+	$%Player.health_component.died.connect(on_player_died)
 
 func on_player_died():
-    var end_screen = end_screen_scene.instantiate()
-    get_tree().current_scene.add_child(end_screen)
-    end_screen.set_defeat()
-    
+	var end_screen = end_screen_scene.instantiate()
+	get_tree().current_scene.add_child(end_screen)
+	end_screen.set_defeat()
+	
